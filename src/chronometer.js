90e283a13fe4b6829fd05cde3d104980daef7726
Giulia Colombo <giulia.colombo.127@gmail.com>
2022-11-30 18:01:52 +0100

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,73 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+
   }
 
-  start(printTimeCallback) {
-    // ... your code goes here
+
+  start(printTimeCallback) { // printTimeCallback - it's like renderEverything
+    this.intervalId = setInterval( () => {
+      //if the printTimeCallback is not passed, it should be disregarded.
+      this.currentTime++;
+
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      //increment seconds in currentTime by 1
+
+    }, 1000)
   }
+ 
 
   getMinutes() {
-    // ... your code goes here
+    let currentTimeMinutes = Math.floor(this.currentTime/60);
+    return currentTimeMinutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let currentTimeSeconds = this.currentTime % 60;
+    return currentTimeSeconds;
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+  computeTwoDigitNumber(value) { //value is an int so we can't iterate!
+    //helper function that will convert any number into a 2-digit string representation
+    //take a number as arg and return a string, where the first digit is 0 if we only have 1 number
+    //eg 7 should be "07"
+    let stringOfValue = "";
+    if (value < 10) {
+      stringOfValue = value.toString().padStart(2,'0')
+      return stringOfValue
+    }
+    else {
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    //should return a string
+    //string showing "mm:ss"format
+    //get the mm
+    let currentMinutes = this.computeTwoDigitNumber(this.getMinutes());
+    let currentSeconds = this.computeTwoDigitNumber(this.getSeconds());
+    let splitTime = `${currentMinutes}:${currentSeconds}`
+    return splitTime;
+
   }
-}
+
+}  
+
+
+
+
+/* const chronometer = new Chronometer();
+
+console.log(chronometer); */
